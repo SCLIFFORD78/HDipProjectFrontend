d15693512ab992e58e72c5cefac491fa66741660
SCLIFFORD78 <faugh01@gmail.com>
2022-03-15 12:09:21 +0000

    clean up of hive detail

--- src/pages/HiveDetail.svelte
+++ src/pages/HiveDetail.svelte
@@ -3,18 +3,6 @@
   import HiveInfo from "../components/HiveInfo.svelte";
   import PhotoWidget from "../components/PhotoWidget.svelte";
   import { getContext, onMount } from "svelte";
-  import { LayerCake, Svg, Html } from "layercake";
-  import Line from "../components/Line.svelte";
-  import Area from "../components/Area.svelte";
-  import AxisX from "../components/AxisX.svelte";
-  import AxisY from "../components/AxisY.svelte";
-  import Brush from "../components/Brush.svelte";
-  import SyncedBrushWrapper from "../components/SyncedBrushWrapper.svelte";
-  import Chart from "svelte-frappe-charts";
-  import { scaleOrdinal } from "d3-scale";
-  import MultiLine from "../components/MultiLine.svelte";
-  import Labels from "../components/GroupLabels.html.svelte";
-  import SharedTooltip from "../components/SharedTooltip.html.svelte";
   import "@carbon/charts/styles.min.css";
   import "carbon-components/css/carbon-components.min.css";
   import { LineChart } from "@carbon/charts-svelte";
@@ -32,55 +20,14 @@
     bar: mainBar,
   });
 
-  let brushExtents = [null, null];
-  let brushedData;
 
-  const xKey = "x";
-  const yKey = "y";
-  const zKey = "z";
-
-  var points = [];
-  var humidityPoints = [];
   var combinedPoints = [];
-  var historicalPoints = [];
-  var historicalHumidityPoints = [];
-  var newData = [];
-  var newHumidityData = [];
-  var historicalData = [];
-  var historicalHumidityData = [];
   var newCombinedData = [];
-
-  var datasets = [newData, historicalData];
-  $: {
-    brushedData = points.slice(
-      (brushExtents[0] || 0) * points.length,
-      (brushExtents[1] || 1) * points.length
-    );
-    console.log(points);
-    if (brushedData.length < 2) {
-      brushedData = points.slice(
-        brushExtents[0] * points.length,
-        brushExtents[0] * points.length + 2
-      );
-    }
-  }
 
   const hive = hiveTracker.selectedHive[0];
   var values = JSON.parse("[" + hive["recordedData"] + "]");
-  var temps = [];
   values.forEach((element) => {
     var theDate = new Date(element["timeStamp"] * 1000);
-    var dateString =
-      theDate.toLocaleDateString() +
-      " " +
-      theDate.getHours() +
-      ":" +
-      theDate.getMinutes() +
-      ":" +
-      theDate.getMinutes();
-    //layer cake chart
-    points.push({ x: element["timeStamp"], y: element["Temperature"] });
-    humidityPoints.push({ x: element["timeStamp"], y: element["Humidity"] });
     combinedPoints.push({
       group: "Hive Temp",
       date: theDate.toISOString(),
@@ -106,23 +53,6 @@
       if (weatherHistory.length > 0) {
         weatherHistory.forEach((element) => {
           var theDate = new Date(element["timeStamp"] * 1000);
-          var dateString =
-            theDate.toLocaleDateString() +
-            " " +
-            theDate.getHours() +
-            ":" +
-            theDate.getMinutes() +
-            ":" +
-            theDate.getMinutes();
-          //layer cake chart
-          historicalPoints.push({
-            x: element["timeStamp"],
-            y: element["Temperature"],
-          });
-          historicalHumidityPoints.push({
-            x: element["timeStamp"],
-            y: element["Humidity"],
-          });
           combinedPoints.push({
             group: "Ambient Temp",
             date: theDate.toISOString(),
@@ -135,33 +65,13 @@
           });
         });
 
-        brushedData = points.slice(
-          (brushExtents[0] || 0) * points.length,
-          (brushExtents[1] || 1) * points.length
-        );
-        if (brushedData.length < 2) {
-          brushedData = points.slice(
-            brushExtents[0] * points.length,
-            brushExtents[0] * points.length + 2
-          );
-        }
       }
     } catch (error) {
       errorMessage = "Weather Details unavailable";
       console.log(error);
     }
-    newData = points;
-    historicalData = historicalPoints;
-    newHumidityData = humidityPoints;
-    historicalHumidityData = historicalHumidityPoints;
     newCombinedData = combinedPoints;
-    datasets = [
-      newData,
-      historicalData,
-      newHumidityData,
-      historicalHumidityData,
-    ];
-    console.log(newCombinedData);
+
   });
 </script>
 
