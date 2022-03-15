fa7e89e0a7309ec0aa3574da26c604b588029233
SCLIFFORD78 <faugh01@gmail.com>
2022-03-15 01:00:01 +0000

    multi brush chatrs working individually

--- src/pages/HiveDetail.svelte
+++ src/pages/HiveDetail.svelte
@@ -9,6 +9,13 @@
   import AxisX from "../components/AxisX.svelte";
   import AxisY from "../components/AxisY.svelte";
   import Brush from "../components/Brush.svelte";
+  import SyncedBrushWrapper from "../components/SyncedBrushWrapper.svelte";
+  import Chart from "svelte-frappe-charts";
+  import { scaleOrdinal } from "d3-scale";
+  import MultiLine from "../components/MultiLine.svelte";
+  import Labels from "../components/GroupLabels.html.svelte";
+  import SharedTooltip from "../components/SharedTooltip.html.svelte";
+
   const hiveTracker = getContext("HiveTracker");
 
   let weather = [];
@@ -26,24 +33,71 @@
 
   const xKey = "x";
   const yKey = "y";
-  var points = [];
-  var historicalPoints = [];
-  var newData = [];
-  var historicalData = [];
+  const zKey = "z";
 
+  var points = [];
+  var humidityPoints = [];
+  var combinedPoints = [];
+  var historicalPoints = [];
+  var historicalHumidityPoints = [];
+  var newData = [];
+  var newHumidityData = [];
+  var historicalData = [];
+  var historicalHumidityData = [];
+  var newCombinedData = [];
+  var temp = [];
+  var temp2 = [];
+  var temp3 = [];
+
+  var datasets = [newData, historicalData];
   $: {
-    brushedData = historicalPoints.slice(
-      (brushExtents[0] || 0) * historicalPoints.length,
-      (brushExtents[1] || 1) * historicalPoints.length
+    brushedData = points.slice(
+      (brushExtents[0] || 0) * points.length,
+      (brushExtents[1] || 1) * points.length
     );
-    console.log(historicalPoints);
+    console.log(points);
     if (brushedData.length < 2) {
       brushedData = points.slice(
-        brushExtents[0] * historicalPoints.length,
-        brushExtents[0] * historicalPoints.length + 2
+        brushExtents[0] * points.length,
+        brushExtents[0] * points.length + 2
       );
     }
   }
+
+  let data = {
+    labels: [],
+    datasets: [
+      {
+        name: "Hive",
+        values: [],
+        lineOptions: {
+          regionFill: 1, // default: 0
+          hideDots: 1,
+        },
+        axisOptions: {
+          xIsSeries: 1, // default: 0
+        },
+      },
+      {
+        name: "Enviroment",
+        values: [],
+        lineOptions: {
+          regionFill: 1, // default: 0
+          hideDots: 1,
+        },
+        axisOptions: {
+          xIsSeries: 1, // default: 0
+        },
+      },
+    ],
+    lineOptions: {
+      regionFill: 1, // default: 0
+      hideDots: 1,
+    },
+    axisOptions: {
+      xIsSeries: 1, // default: 0
+    },
+  };
 
   const hive = hiveTracker.selectedHive[0];
   var values = JSON.parse("[" + hive["recordedData"] + "]");
@@ -60,6 +114,12 @@
       theDate.getMinutes();
     //layer cake chart
     points.push({ x: element["timeStamp"], y: element["Temperature"] });
+    humidityPoints.push({ x: element["timeStamp"], y: element["Humidity"] });
+    combinedPoints.push({
+      x: element["timeStamp"],
+      y: element["Temperature"],
+      z: null,
+    });
   });
 
   onMount(async () => {
@@ -84,25 +144,71 @@
             ":" +
             theDate.getMinutes();
           //layer cake chart
-          historicalPoints.push({ x: element["timeStamp"], y: element["Temperature"] });
+          historicalPoints.push({
+            x: element["timeStamp"],
+            y: element["Temperature"],
+          });
+          historicalHumidityPoints.push({
+            x: element["timeStamp"],
+            y: element["Humidity"],
+          });
+          combinedPoints.push({
+            x: element["timeStamp"],
+            y: null,
+            z: element["Temperature"],
+          });
         });
+        combinedPoints.sort((a, b) => (a.x > b.x ? 1 : -1));
+        let tempz = 0;
+        let tempy = 0;
+        for (let index = 0; index < combinedPoints.length; index++) {
+          const element = combinedPoints[index];
+          temp.push(element["x"]);
+          if (element["y"]) {
+            tempy = element["y"];
+            temp2.push(element["y"]);
+          } else {
+            temp2.push(tempy);
+          }
+          if (element["z"]) {
+            tempz = element["z"];
+            temp3.push(element["z"]);
+          } else {
+            temp3.push(tempz);
+          }
+        }
+        brushedData = points.slice(
+          (brushExtents[0] || 0) * points.length,
+          (brushExtents[1] || 1) * points.length
+        );
+        if (brushedData.length < 2) {
+          brushedData = points.slice(
+            brushExtents[0] * points.length,
+            brushExtents[0] * points.length + 2
+          );
+        }
+
+        data.labels = temp;
+        data.datasets[0].values = temp2;
+        data.datasets[1].values = temp3;
+        console.log(data);
       }
     } catch (error) {
       errorMessage = "Weather Details unavailable";
       console.log(error);
     }
     newData = points;
-    historicalData = historicalPoints
-    brushedData = historicalPoints.slice(
-      (brushExtents[0] || 0) * historicalPoints.length,
-      (brushExtents[1] || 1) * historicalPoints.length
-    );
-    if (brushedData.length < 2) {
-      brushedData = points.slice(
-        brushExtents[0] * historicalPoints.length,
-        brushExtents[0] * historicalPoints.length + 2
-      );
-    }
+    historicalData = historicalPoints;
+    newHumidityData = humidityPoints;
+    historicalHumidityData = historicalHumidityPoints;
+    newCombinedData = combinedPoints;
+    datasets = [
+      newData,
+      historicalData,
+      newHumidityData,
+      historicalHumidityData,
+    ];
+    console.log(newCombinedData);
   });
 </script>
 
@@ -129,6 +235,23 @@
     </div>
   </div>
 </div>
+
+<div class="uk-child-width-1-2@s" uk-grid>
+  {#each datasets as dataset}
+    <div
+      class="uk-height-large uk-card uk-card-default uk-card-body uk-flex uk-flex-center uk-flex-middle"
+    >
+      <SyncedBrushWrapper
+        data={dataset}
+        {xKey}
+        {yKey}
+        bind:min={brushExtents[0]}
+        bind:max={brushExtents[1]}
+      />
+    </div>
+  {/each}
+</div>
+
 <div
   class="uk-card uk-card-default uk-card-large uk-card-body uk-box-shadow-large uk-width-3xlarge uk-margin uk-height-2xlarge uk-align-center"
 >
@@ -140,6 +263,7 @@
       padding={{ right: 10, bottom: 20, left: 25 }}
       x={xKey}
       y={yKey}
+      z={zKey}
       data={brushedData}
     >
       <Svg>
@@ -162,7 +286,7 @@
   <div
     class="uk-padding uk-child-width-expand@s uk-text-center uk-height-small uk-align-center"
   >
-    <LayerCake padding={{ top: 5 }} x="x" y="y" data={historicalData}>
+    <LayerCake padding={{ top: 5 }} x="x" y="y" data={newData}>
       <Svg>
         <Line stroke="#00e047" />
         <Area fill="#00e04710" />
