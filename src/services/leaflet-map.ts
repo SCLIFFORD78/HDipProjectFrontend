4163210c074736cad59fa47592f130d429ced8e2
SCLIFFORD78 <faugh01@gmail.com>
2022-04-04 01:33:18 +0100

    image issues deployment

--- src/services/leaflet-map.ts
+++ src/services/leaflet-map.ts
@@ -19,7 +19,7 @@ export interface MapConfig {
 }
 
 var myIcon = L.icon({
-  iconUrl: 'src\\assets\\bumblebee.jpg',
+  iconUrl: 'public\\images\\bumblebee.jpg',
   iconSize: [48, 48],
   iconAnchor: [24, 36],
   popupAnchor: [-3, -76]
